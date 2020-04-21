3aca2e28c606d730683d44c0efc718ebf8c73d46
global-event-2762 <rosalinschlosser@gmail.com>
2020-04-21 18:51:44 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,55 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = Song.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = Song.new;
+    song.name = name;
+    song
+  end
+
+  def self.create_by_name(name)
+    song = Song.new
+    song.name = name
+    song.save
+    song
+  end
+
+  def self.find_by_name(name)
+    self.all.find {|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name)
+      self.find_by_name(name)
+    else
+      self.create_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    self.all.sort{|a, b| a.name <=> b.name}
+  end
+
+  def self.new_from_filename(filename)
+    file_arr = filename.delete_suffix(".mp3").split(" - ")
+    song = self.create_by_name(file_arr[1])
+    song.artist_name = file_arr[0]
+    song
+  end
+
+  def self.create_from_filename(filename)
+    song = self.new_from_filename(filename)
+    song.save
+  end
+
+  def self.destroy_all
+    self.all.clear;
+  end
+
 end
