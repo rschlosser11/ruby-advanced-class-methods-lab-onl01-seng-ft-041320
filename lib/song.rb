74b279f5df20ea03bc2bab2ac384c70625a22a4b
global-event-2762 <rosalinschlosser@gmail.com>
2020-04-21 16:51:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,7 +34,11 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    
+    if self.all.find {|song| song.name == name}
+      song
+    else
+      self.create_by_name
+    end
   end
 
 end
